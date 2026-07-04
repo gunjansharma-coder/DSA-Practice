6c679486eccc464187875442addae715e484c01d
Gunjan Sharma <gunjansharma6693@gmail.com>
2026-07-04 23:29:39 +0530

    Modified version of left rotation of the array

--- Arrays/Easy/rotate_left.py
+++ Arrays/Easy/rotate_left.py
@@ -1,13 +1,21 @@
+# Problem: Remove Duplicates from Sorted Array
+# Topic: Arrays
+# Difficulty: Easy
+# Source: LeetCode
+
 class Solution:
-    def rotate(self, nums, k: int) -> None:
-        n=len(nums)
-        for i in range(0,n):
-            if k>0:
-                temp=nums[i]
-                nums[i]=nums[-k]
-                nums[-k]=temp
-                k-=1
-        return nums
-obj=Solution()
-z=obj.rotate([1,2,3,4,5,6,7],3)
-print(z)
+
+class Solution:
+    def rotate(self, nums, k):
+        n = len(nums)
+        k %= n
+
+        # Reverse the whole array
+        nums.reverse()
+
+        # Reverse first k elements
+        nums[:k] = reversed(nums[:k])
+
+        # Reverse remaining elements
+        nums[k:] = reversed(nums[k:])
+
